bfba2123818b20e1078c8cfd17bfd4d355ae5462
aymane-smi <aymanebel2@outlook.fr>
2023-02-15 17:25:45 +0100

    add migration and table

--- database/migrations/2023_02_15_130920_create_client.php
+++ database/migrations/2023_02_15_130920_create_client.php
@@ -11,10 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->string("fname");
+            $table->string("lname");
+            $table->string("password");
+            $table->string("email");
+            $table->timestamps();
         });
     }
 
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('client');
     }
 };
