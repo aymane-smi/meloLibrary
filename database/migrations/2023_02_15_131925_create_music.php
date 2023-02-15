b086684b1017f916de68a7fc5c3f81209e5b2046
aymane-smi <aymanebel2@outlook.fr>
2023-02-15 17:25:31 +0100

    add migration and table

---
+++ database/migrations/2023_02_15_131925_create_music.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('music', function (Blueprint $table) {
+            $table->id();
+            $table->string("image");
+            $table->string("title");
+            $table->foreignId("artist_id")->nullable()->constrained();
+            $table->foreignId("band_id")->nullable()->constrained();
+            $table->foreignId("writer_id")->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('music');
+    }
+};
