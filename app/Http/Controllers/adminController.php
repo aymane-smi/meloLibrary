60c5d94ede4a07376093bc30ac5560498bb2a2d9
aymane-smi <aymanebel2@outlook.fr>
2023-02-21 16:57:17 +0100

    making dashboard controller

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\artist;
 use App\Models\band;
+use App\Models\category;
 use App\Models\music;
 use Illuminate\Http\Request;
 
@@ -14,10 +15,17 @@ class adminController extends Controller
     public function index()
     {
         $result = [
-            "created_songs" => music::count(),
-            "created_artists" => artist::count(),
-            "created_band" => band::count(),
-            "top_songs" => music
+            "created_songs" => music::count()->get(),
+            "created_artists" => artist::count()->get(),
+            "created_band" => band::count()->get(),
+            "top_songs" => music::select('music.*')->join('music_rate', 'music.id', '=', 'music_rate.music_id')
+                ->groupBy("music.*")
+                ->orderBy("rate")
+                ->limit(4)
+                ->get(),
+            "top_categories" => category::all()->orderBy("selected"),
+            "top_artists" => artist::select("artist.*")->join('artist_music', 'artist.id', '=', 'artist_music.artist_id')
+                ->join("artist", 'artist.id', '=', ''),
         ];
         return view("admin.index");
     }
